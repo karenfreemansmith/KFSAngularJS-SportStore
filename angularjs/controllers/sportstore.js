f4195aca7684d157162cb1ac3bf2777b9b031d3c
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-07-31 16:25:02 -0700

    display product details

--- angularjs/controllers/sportstore.js
+++ angularjs/controllers/sportstore.js
@@ -1,4 +1,15 @@
 angular.module("sportstore")
 .controller("sportstoreCtrl", function($scope) {
-  
+  $scope.data = {
+    products: [
+      { name: "Product #1", description: "A product",
+        category: "Category #1", price: 100 },
+      { name: "Product #2", description: "A product",
+        category: "Category #1", price: 110 },
+      { name: "Product #3", description: "A product",
+        category: "Category #2", price: 200 },
+      { name: "Product #4", description: "A product",
+        category: "Category #2", price: 210 }
+    ]
+  };
 });
