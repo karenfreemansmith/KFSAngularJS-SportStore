e7eec1120fb3925567cdafae2de540cfef996ca1
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-07-31 17:17:02 -0700

    create and apply category filter

---
+++ angularjs/controllers/productListControllers.js
@@ -0,0 +1,10 @@
+angular.module("sportstore")
+  .controller("productListCtrl", function($scope, $filter) {
+    var selectedCategory = null;
+    $scope.selectCategory = function(newCategory) {
+      selectedCategory = newCategory;
+    }
+    $scope.categoryFilterFn = function(product) {
+      return selectedCategory == null || product.category == selectedCategory;
+    }
+  });
